351595efcbacbdc528ea0658d32d1656ceae0385
Smeet Hendre <smeethendre4@gmail.com>
2025-06-28 02:38:23 +0530

    type

--- src/util/asyncHandler.js
+++ src/util/asyncHandler.js
@@ -1,6 +1,6 @@
 const asyncHandler = (fn) => async (req, res, next) => {
     try {
-        await fu(req, res, next)
+        await fn(req, res, next)
     } catch (error) {
         res.status(error.code || 500).json({
             success: false, 
