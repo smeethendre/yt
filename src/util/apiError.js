9bea4cf57d1de95e1a787dc7a3152acb69f7aae5
Smeet Hendre <smeethendre4@gmail.com>
2025-07-03 16:52:08 +0530

    refactor(ApiError): improve constructor formatting and fix typo in stack parameter

--- src/util/apiError.js
+++ src/util/apiError.js
@@ -1,13 +1,23 @@
-class ApiError extends Error{
-    constructor(statusCode, message = "Something went wrong", error=[], statck=""){
+class ApiError extends Error {
+  constructor(
+    statusCode,
+    message = "Something went wrong",
+    error = [],
+    stack = ""
+  ) {
+    (super(message),
+      (this.statusCode = statusCode),
+      (this.success = false),
+      (this.data = null),
+      (this.error = error),
+      (this.name = "ApiError"));
 
-        super(message),
-        this.statusCode = statusCode,
-        this.success = false, 
-        this.data = null, 
-        this.error = error, 
-        this.name = "ApiError"
+    if (stack) {
+      this.stack = stack;
+    } else {
+      Error.captureStackTrace(this, this.constructor);
     }
+  }
 }
 
-export {ApiError}
+export { ApiError };
