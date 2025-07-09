8a507244233e8e60e217158f998dd13231c0f1cc
Smeet Hendre <smeethendre4@gmail.com>
2025-07-09 23:28:24 +0530

    feat(user): enhance user registration with input validation and file upload support

--- src/controller/user.controller.js
+++ src/controller/user.controller.js
@@ -1,10 +1,27 @@
 import { asyncHandler } from "../util/asyncHandler.js";
 import { uploadToCloudinary } from "../util/cloudinary.js";
 import { User } from "../model/user.model.js";
+import {ApiError} from "../util/apiError.js";
 
 const registerUser = asyncHandler(async (req, res) => {
   const { fullName, email, userName, password } = req.body;
-  console.log("email: ", email);
+  console.log(fullName, email, userName, password);
+
+  if(fullName === ""){
+    throw new ApiError(400,"Please enter your full name");
+  }
+
+  if(email === ""){
+    throw new ApiError(400, "Please enter your full email address")
+  }
+
+  if(userName === ""){
+    throw new ApiError(400, "Please enter your complete user-name")
+  }
+
+  if(password === ""){
+    throw new ApiError(400, "Please enter password")
+  }
   
 });
 
